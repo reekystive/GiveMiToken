b9ed99e075585c9c864282c9821f2d590c46fbda
Mingxuan Wang <wangmingxuan.io@bytedance.com>
2025-04-27 23:35:01 +0800

    chore: ✨ add vite-tsconfig-paths plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
